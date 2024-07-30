4fe39fb05bc4a2d0a3ae97e5398775ba62c04b24
Kona Fatsea <fatseakona@gmail.com>
2024-07-30 09:12:12 +0200

    feat: add user input function
    - Added get_bet() function to get the bet amount on each line
    - Updated main() function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 # Collect user input
 def deposit():
@@ -32,10 +34,28 @@ def get_number_of_lines():
      return lines
 
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+                if MIN_BET <= amount <= MAX_BET:
+                    break
+                else:
+                    print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+            
+    
+    return amount
+
+
 def main(): #If we end our program we call main to rerun the program
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    bet = get_bet()
+    total_bet = lines * bet
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 
 main()
